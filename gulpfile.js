4d7774cc9dbe67cf68681ec3581ec8dc91b5ed82
Stephanie Zeng <stephaniezeng521@gmail.com>
2017-01-14 10:46:44 -0500

    Installed gulp and gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+
+// style task
+// Sass
 gulp.task('sass', function(){
-  return gulp.src('./app/assets/styles/styles.scss')
-    .pipe(sass().on('error', sass.logError))
+  return gulp.src('./app/assets/styles/**/*.scss')
+    .pipe(sass({
+      style:'compressed'
+    }).on('error', sass.logError))
     .pipe(gulp.dest('./app/temp/styles'));
 });
 
